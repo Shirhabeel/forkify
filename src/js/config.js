b73309d800052a7f452ab1c823a3decd5c83fdd8
shirhabeel <shirhabeel5185@gmail.com>
2023-09-30 18:33:25 +0500

    Inital commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6d23c31e-bed0-46d8-9cec-3c52a0256697';
+export const MODAL_CLOSE_SEC = 2.5;
